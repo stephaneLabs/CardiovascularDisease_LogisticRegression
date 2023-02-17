3aa8627deb80d2af4746cd763f5bb2f99c8621de
Stéphane L <98824978+stephaneLabs@users.noreply.github.com>
2023-02-17 14:44:20 +0100

    Kaggle Notebook | Cardiovascular Disease Logistic Regression R | Version 3

--- notebook9fa826d052.r
+++ notebook9fa826d052.r
@@ -3,21 +3,21 @@
   {
    "cell_type": "code",
    "execution_count": 1,
-   "id": "c41a4709",
+   "id": "0009a5f8",
    "metadata": {
     "_execution_state": "idle",
     "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
     "execution": {
-     "iopub.execute_input": "2023-02-17T13:39:49.065276Z",
-     "iopub.status.busy": "2023-02-17T13:39:49.063341Z",
-     "iopub.status.idle": "2023-02-17T13:39:49.203364Z",
-     "shell.execute_reply": "2023-02-17T13:39:49.201394Z"
+     "iopub.execute_input": "2023-02-17T13:44:11.312846Z",
+     "iopub.status.busy": "2023-02-17T13:44:11.311101Z",
+     "iopub.status.idle": "2023-02-17T13:44:11.422261Z",
+     "shell.execute_reply": "2023-02-17T13:44:11.420856Z"
     },
     "papermill": {
-     "duration": 0.147236,
-     "end_time": "2023-02-17T13:39:49.206582",
+     "duration": 0.117343,
+     "end_time": "2023-02-17T13:44:11.424708",
      "exception": false,
-     "start_time": "2023-02-17T13:39:49.059346",
+     "start_time": "2023-02-17T13:44:11.307365",
      "status": "completed"
     },
     "tags": []
@@ -54,14 +54,14 @@
   },
   "papermill": {
    "default_parameters": {},
-   "duration": 3.893161,
-   "end_time": "2023-02-17T13:39:49.328682",
+   "duration": 3.153442,
+   "end_time": "2023-02-17T13:44:11.546083",
    "environment_variables": {},
    "exception": null,
    "input_path": "__notebook__.ipynb",
    "output_path": "__notebook__.ipynb",
    "parameters": {},
-   "start_time": "2023-02-17T13:39:45.435521",
+   "start_time": "2023-02-17T13:44:08.392641",
    "version": "2.4.0"
   }
  },
