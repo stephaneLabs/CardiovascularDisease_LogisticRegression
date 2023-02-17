4a1ed1bed4c8cb480f1d6ab7f4a5dbd9c6e8ea77
Stéphane L <98824978+stephaneLabs@users.noreply.github.com>
2023-02-17 14:38:41 +0100

    Kaggle Notebook | notebook9fa826d052 | Version 1

---
+++ notebook9fa826d052.r
@@ -0,0 +1,70 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": 1,
+   "id": "e2182776",
+   "metadata": {
+    "_execution_state": "idle",
+    "_uuid": "051d70d956493feee0c6d64651c6a088724dca2a",
+    "execution": {
+     "iopub.execute_input": "2023-02-17T13:38:30.776378Z",
+     "iopub.status.busy": "2023-02-17T13:38:30.774020Z",
+     "iopub.status.idle": "2023-02-17T13:38:30.937602Z",
+     "shell.execute_reply": "2023-02-17T13:38:30.935435Z"
+    },
+    "papermill": {
+     "duration": 0.171351,
+     "end_time": "2023-02-17T13:38:30.940758",
+     "exception": false,
+     "start_time": "2023-02-17T13:38:30.769407",
+     "status": "completed"
+    },
+    "tags": []
+   },
+   "outputs": [],
+   "source": [
+    "# logisticRegressionAddons_20230217.R                     : addons to assess a logistic regression fit\n",
+    "# Cardiovascular_Disease_Logistic_Regression_20230217.Rmd : Analysis using a dataset from Kaggle\n",
+    "# \n",
+    "# The scripts are available at :\n",
+    "# https://github.com/stephaneLabs/CardiovascularDisease_LogisticRegression\n",
+    "#\n",
+    "#    The .R and .Rmd scripts are under GPL3 Licence.\n",
+    "#\n",
+    "# The dataset is available on Kaggle :\n",
+    "# https://www.kaggle.com/code/maryamsayagh1/cardiovascular-disease-prediction/data\n",
+    "#\n"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "R",
+   "language": "R",
+   "name": "ir"
+  },
+  "language_info": {
+   "codemirror_mode": "r",
+   "file_extension": ".r",
+   "mimetype": "text/x-r-source",
+   "name": "R",
+   "pygments_lexer": "r",
+   "version": "4.0.5"
+  },
+  "papermill": {
+   "default_parameters": {},
+   "duration": 5.025522,
+   "end_time": "2023-02-17T13:38:31.073505",
+   "environment_variables": {},
+   "exception": null,
+   "input_path": "__notebook__.ipynb",
+   "output_path": "__notebook__.ipynb",
+   "parameters": {},
+   "start_time": "2023-02-17T13:38:26.047983",
+   "version": "2.4.0"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
